acc8087af3aa294eb1bab17ca99ba940a2776a4a
Rastogi-thecoder-9 <167659485+Rastogi-thecoder-9@users.noreply.github.com>
2024-04-21 07:37:10 +0530

    Update factory.ts by Anshul Rastogi

--- src/factory.ts
+++ src/factory.ts
@@ -5,4 +5,14 @@
  * @param {number} [step] - The step to increment each number in the sequence. Default is 1.
  * @returns {Function} - A function that generates the number sequence every time it is called.
  */
-export const factory = (start?: number, step?: number) => {};
+export const factory = (start?: number, step?: number):() => {
+  if(start){
+  return start;
+}
+  else if(step){
+  return step;
+}
+ else if(start && step){ 
+  return start + step
+ }
+};
